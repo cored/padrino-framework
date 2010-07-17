ae942fc61e540aaf84bb5dcb9f2161312a0b034b
Rafael George <george.rafael@gmail.com>
2010-07-16 23:21:09 -0400

    added flexmock handler file

---
+++ padrino-gen/lib/padrino-gen/generators/components/mocks/flexmock.rb
@@ -0,0 +1,7 @@
+def setup_mock
+    require_dependencies 'flexmock', :group => 'test'
+    case options[:test].to_s
+        when 'rspec'
+            inject_into_file 'spec/spec_helper.rb', " conf.mock_with :flexmock\n", :afer => "Spec:Runner.configure do |conf|\n"
+    end
+end
